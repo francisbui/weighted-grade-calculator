86cd5a1ec312be9c96e02265b2bdded05dca0d0f
Francis Bui <francisvbui@gmail.com>
2020-12-17 12:17:23 -0500

    Initial commit

---
+++ weightcalc.tcl
@@ -0,0 +1,9 @@
+
+    while executing
+"vTcl:exit"
+    (procedure "vTcl:display_error" line 27)
+    invoked from within
+"vTcl:display_error $errorInfo"
+    (procedure "bgerror" line 4)
+    invoked from within
+"bgerror {}"
